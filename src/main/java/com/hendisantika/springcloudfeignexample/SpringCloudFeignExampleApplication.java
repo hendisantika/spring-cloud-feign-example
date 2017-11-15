26aa70177c867bbce0021770442ec71abb0b403c
Hendi Santika <hendisantika@gmail.com>
2017-11-15 08:06:54 +0700

    #4 - Add annotation in Main App

--- src/main/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplication.java
+++ src/main/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplication.java
@@ -2,11 +2,20 @@ package com.hendisantika.springcloudfeignexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class SpringCloudFeignExampleApplication {
+@EnableWebMvc
+@Controller
+@EnableFeignClients
+public class SpringCloudFeignExampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFeignExampleApplication.class, args);
 	}
+
+    
 }
