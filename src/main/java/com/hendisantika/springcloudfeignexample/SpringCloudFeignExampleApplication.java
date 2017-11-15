25193713cc35ffaa477c45e72a925d19a1160c40
Hendi Santika <hendisantika@gmail.com>
2017-11-15 07:59:24 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springcloudfeignexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFeignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springcloudfeignexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudFeignExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
