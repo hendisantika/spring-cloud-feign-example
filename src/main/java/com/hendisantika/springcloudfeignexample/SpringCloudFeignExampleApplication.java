78dad2b4eb4dd113eec167cdcdb1377c3b9afdac
Hendi Santika <hendisantika@gmail.com>
2017-11-15 08:09:11 +0700

    #7 - Modified Main App

--- src/main/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplication.java
+++ src/main/java/com/hendisantika/springcloudfeignexample/SpringCloudFeignExampleApplication.java
@@ -1,11 +1,17 @@
 package com.hendisantika.springcloudfeignexample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -17,5 +23,17 @@ public class SpringCloudFeignExampleApplication extends SpringBootServletInitial
 		SpringApplication.run(SpringCloudFeignExampleApplication.class, args);
 	}
 
+    @Autowired
+    private GitHubClient gitHub;
 
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/{owner}/{repo}")
+    @ResponseBody
+    public List<Contributor> contributors(@PathVariable String owner, @PathVariable String repo) {
+        return gitHub.contributors(owner, repo);
+    }
 }
